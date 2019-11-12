0641766975c2ee6c10ca12df0028342c15d9c81f
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-11-12 15:34:28 -0500

    Day 2, set up sessionConfig

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const cors = require('cors');
- authRouter = require ('./auth/users-router.js')
+const session = require('express-session');
+
+authRouter = require('./auth/users-router.js')
 
 const server = express();
 server.use(cors());
-server.use(express.json ());
+server.use(session());
+server.use(express.json());
 
-server.use('/auth',authRouter);
+server.use('/auth', authRouter);
 
-
+const sessionConfiguration = {
+    name: 'name',
+    secret: process.env.COOKIE_SECRET || 'key',
+    cookie: {
+        maxAge: 1000 * 60 * 60,
+        secure: process.env.NODE_ENV === "production" ? true : false,
+        httpOnly: true
+    },
+    resave: false,
+    saveUninitialized: true,
+}
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`\n** Running on port: ${PORT} **\n`));
